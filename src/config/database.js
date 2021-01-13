f86e2524f4d0ac1a0b5c33e22b583c7277f58a0f
Bruno R.Novais <novais.rezende@gmail.com>
2021-01-12 21:11:24 -0300

    Done db connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,9 @@
 module.exports = {
-  dialect: 'sqlite',
+  host: '127.0.0.1',
+  username: 'postgres',
+  password: '1234',
+  database: 'nodeauth',
+  dialect: 'postgres',
   storage: '../database/database.sqlite',
   operatorAliases: false,
   logging: false,
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
 const routes = require('express').Router();
+const { User } = require('./app/models');
 
 module.exports = routes;
